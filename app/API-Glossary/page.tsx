8f24066f9a083bd982c4c9229b79e19006c3fa40
Khoi1909 <kn24524@gmail.com>
2024-12-20 00:26:59 +0700

    API-Glossary Page

---
+++ app/API-Glossary/page.tsx
@@ -0,0 +1,252 @@
+export default function GlossaryPage(){
+    return (
+        <article className="leading-loose relative justify-center min-h-screen mb-12" id="Article-Glossary">
+            <h1 className="text-zinc-700 text-5xl font-bold mb-4"><strong data-renderer-mark="true">APILayer API Glossary: Glossary of API Terms, Definitions &amp; Programming Acronyms</strong></h1>
+            <h2 className="text-zinc-700 text-xl font-semibold mb-3">What is the API Glossary?</h2>
+            <p className="text-black text-sm mb-2">APILayer’s API glossary contains an alphabetical list of API-related special terms, definitions, acronyms, and related phrases to help developers understand the terminology.&nbsp; Browse the APILayer glossary and learn to speak the language of professional API developers!&nbsp;</p>
+            <h2 className="text-black text-2xl font-bold " id="A">A</h2>
+            <ul className="list-disc pl-4 ml-5 text-black">
+                <li id="list"><a href="https://blog.apilayer.com/access-key-what-is-access-key/">Access Key</a></li>
+                <li id="list"><a
+                    href="https://blog.apilayer.com/api-glossary-introduction-to-the-application-programming-interfaces/#ajax">AJAX</a>
+                </li>
+                <li id="list"><a href="https://blog.apilayer.com/what-is-an-api/">API</a></li>
+                <li id="list">API</li>
+                <li id="list"><a
+                    href="https://blog.apilayer.com/api-glossary-introduction-to-the-application-programming-interfaces/#api-access">API
+                    Access</a></li>
+                <li id="list">API Endpoints</li>
+                <li id="list"><a
+                    href="https://blog.apilayer.com/microservices-design-api-gateway-pattern/#:~:text=What%20is%20an%20API%20Gateway%20Pattern%3F">API
+                    Gateway Pattern</a></li>
+                <li id="list">API integration</li>
+                <li id="list">API Keys</li>
+                <li id="list">API management</li>
+                <li id="list">API marketplace</li>
+                <li id="list">API Request</li>
+                <li id="list">API Requests</li>
+                <li id="list">API Security</li>
+                <li id="list">Asn</li>
+                <li id="list">API Testing</li>
+                <li id="list"><a
+                    href="https://blog.apilayer.com/api-glossary-introduction-to-the-application-programming-interfaces/">Application
+                    Programming Interfaces</a></li>
+                <li id="list"><a
+                    href="https://blog.apilayer.com/api-glossary-introduction-to-the-application-programming-interfaces/#authentication">Authentication</a>
+                </li>
+                <li id="list">Automated Web Scraping</li>
+                <li id="list">Automation Through APIs</li>
+            </ul>
+            <h2 className="text-black text-2xl font-bold" id="A">B</h2>
+            <ul className="list-disc pl-4  ml-5 text-black">
+                <li id="list">Base Currency</li>
+                <li id="list"><a href="https://blog.apilayer.com/base-url-what-is-base-url/">Base Url</a></li>
+                <li id="list">Bounce Rate</li>
+                <li id="list"><a href="https://blog.apilayer.com/bad-words-detection-api-use-cases-and-examples/">Bad Words
+                    Detection API</a></li>
+            </ul>
+            <h2 className="text-black text-2xl font-bold" id="A">C</h2>
+            <ul className="list-disc pl-4  ml-5 text-black">
+                <li id="list"><a
+                    href="https://blog.apilayer.com/api-glossary-introduction-to-the-application-programming-interfaces/#client">Client</a>
+                </li>
+                <li id="list"><a
+                    href="https://blog.apilayer.com/easily-monitor-cryptocurrency-prices-using-python-and-rest-services/#:~:text=What%20is%20coinlayer%2C%20and%20What%20Makes%20it%20Powerful%3F">coinlayer</a>
+                </li>
+                <li id="list"><a
+                    href="https://blog.apilayer.com/api-glossary-introduction-to-the-application-programming-interfaces/#crud">CRUD</a>
+                </li>
+                <li id="list">Cryptocurrency Exchange Rates</li>
+                <li id="list"><a
+                    href="https://blog.apilayer.com/api-glossary-introduction-to-the-application-programming-interfaces/#curl">cURL</a>
+                </li>
+                <li id="list"><a
+                    href="https://blog.apilayer.com/how-to-integrate-exchange-rates-and-currency-conversion-in-nodejs/#:~:text=What%20is%20CurrencyLayer%3F">CurrencyLayer</a>
+                </li>
+                <li id="list"><a href="https://blog.apilayer.com/a-beginners-guide-to-a-current-weather-api/">Current Weather
+                    API</a></li>
+                <li id="list">Currencies</li>
+                <li id="list"><a href="https://blog.apilayer.com/country-code-what-is-a-country-code/">Country Code</a></li>
+                <li id="list">Conversion Endpoint</li>
+                <li id="list">Currency Conversion</li>
+            </ul>
+            <h2 className="text-black text-2xl font-bold" id="A">D</h2>
+            <ul className="list-disc pl-4  ml-5 text-black">
+                <li id="list"><a href="https://blog.apilayer.com/did-you-mean-this-api-use-cases-and-examples/">“Did You Mean”
+                    Algorithm</a></li>
+                <li id="list">DDoS</li>
+                <li id="list">dynamic IP address</li>
+            </ul>
+            <h2 className="text-black text-2xl font-bold" id="A">E</h2>
+            <ul className="list-disc pl-4  ml-5 text-black">
+                <li id="list">Email Verification API</li>
+                <li id="list">Endpoint (Api Endpoint)</li>
+                <li id="list">Exchange Rate</li>
+            </ul>
+            <h2 className="text-black text-2xl font-bold" id="A">F</h2>
+            <ul className="list-disc pl-4  ml-5 text-black">
+                <li id="list">FinanceLayer API</li>
+                <li id="list">Fixer</li>
+                <li id="list"><a
+                    href="https://blog.apilayer.com/working-with-apis-in-flutter/#:~:text=API%20on%20Flutter.-,What%20is%20Flutter,-Flutter%20is%20a">Flutter</a>
+                </li>
+                <li id="list">Fluctuation Endpoint</li>
+            </ul>
+            <h2 className="text-black text-2xl font-bold" id="A">G</h2>
+            <ul className="list-disc pl-4  ml-5 text-black">
+                <li id="list"><a
+                    href="https://blog.apilayer.com/ultimate-geocoding-api-integration-with-node-js/#:~:text=December%202%2C%202021-,What%20is%20Geocoding%3F,-Geocoding%20is%20the">Geocoding</a>
+                </li>
+                <li id="list">Google Cloud IoT Core</li>
+                <li id="list"><a
+                    href="https://blog.apilayer.com/google-serp-101-a-complete-guide-to-google-search-result-scraping/">Google
+                    Search Result Scraping</a></li>
+                <li id="list"><a
+                    href="https://blog.apilayer.com/what-is-gpt-3-and-how-can-we-benefit-from-it/#:~:text=cases%20and%20examples-,What%20is%20GPT%2D3%3F,-Let%20us%20take">GPT-3</a>
+                </li>
+            </ul>
+            <h2 className="text-black text-2xl font-bold" id="A">H</h2>
+            <ul className="list-disc pl-4  ml-5 text-black">
+                <li id="list"><a
+                    href="https://blog.apilayer.com/api-glossary-introduction-to-the-application-programming-interfaces/#http">HTTP</a>
+                </li>
+                <li id="list"><a
+                    href="https://blog.apilayer.com/api-glossary-introduction-to-the-application-programming-interfaces/#https">HTTPS</a>
+                </li>
+                <li id="list"><a href="https://blog.apilayer.com/historical-data-what-is-historical-data/">Historical Data</a>
+                </li>
+                <li id="list">Http Request Methods</li>
+                <li id="list">Https Encryption</li>
+            </ul>
+            <h2 className="text-black text-2xl font-bold" id="A">I</h2>
+            <ul className="list-disc pl-4  ml-5 text-black">
+                <li id="list">Image Assessment</li>
+                <li id="list">ICANN</li>
+                <li id="list">IP By Location</li>
+                <li id="list">IP to Geolocation API</li>
+                <li id="list"><a
+                    href="https://blog.apilayer.com/how-do-i-look-up-ip-addresses-with-python-and-flask/#:~:text=What%20is%20ipapi%20%26%20why%20do%20I%20need%20this%20for%20my%20projects%3F">ipapi</a>
+                </li>
+                <li id="list">ipstack</li>
+                <li id="list">IP Address</li>
+                <li id="list">ipv4</li>
+                <li id="list">ipv6</li>
+                <li id="list">Isp</li>
+            </ul>
+            <h2 className="text-black text-2xl font-bold" id="A">J</h2>
+            <ul className="list-disc pl-4  ml-5 text-black">
+                <li id="list"><a
+                    href="https://blog.apilayer.com/api-glossary-introduction-to-the-application-programming-interfaces/#json">JSON</a>
+                </li>
+                <li id="list"><a href="https://blog.apilayer.com/jsonp-callbacks-what-is-a-jsonp-callback/">Jsonp Callbacks</a>
+                </li>
+            </ul>
+            <h2 className="text-black text-2xl font-bold" id="A">L</h2>
+            <ul className="list-disc pl-4  ml-5 text-black">
+                <li id="list">Languagelayer</li>
+                <li id="list"><a
+                    href="https://blog.apilayer.com/create-rest-api-in-laravel-with-authentication-using-passport/#:~:text=package%20of%20Laravel.-,What%20is%20Laravel,-Laravel%20is%20a">Laravel</a>
+                </li>
+            </ul>
+            <h2 className="text-black text-2xl font-bold" id="A">M</h2>
+            <ul className="list-disc pl-4  ml-5 text-black">
+                <li id="list">mailboxlayer</li>
+                <li id="list"><a
+                    href="https://blog.apilayer.com/how-to-create-a-news-web-app-in-node-js-with-a-media-api/#:~:text=of%20the%20article!-,What%20is%20MediaStack%3F,-MediaStack%20API%20is">MediaStack</a>
+                </li>
+                <li id="list"><a href="https://blog.apilayer.com/metadata/">Meta Data</a></li>
+                <li id="list">Multiple Locations</li>
+            </ul>
+            <h2 className="text-black text-2xl font-bold" id="A">N</h2>
+            <ul className="list-disc pl-4  ml-5 text-black">
+                <li id="list"><a
+                    href="https://blog.apilayer.com/how-to-create-a-phone-number-verification-web-app-using-node-js/#:~:text=What%20is%20Numverify%20API%3F">Numverify
+                    API</a></li>
+            </ul>
+            <h2 className="text-black text-2xl font-bold" id="A">P</h2>
+            <ul className="list-disc pl-4  ml-5 text-black">
+                <li id="list">Parameters</li>
+                <li id="list"><a href="https://blog.apilayer.com/ping-what-is-ping/">Ping</a></li>
+                <li id="list">private ip address</li>
+                <li id="list">public ip address</li>
+                <li id="list"><a
+                    href="https://blog.apilayer.com/how-to-get-started-with-powerful-a-geo-api-service-using-nodejs/#:~:text=What%20is%20positionstack">positionstack</a>
+                </li>
+            </ul>
+            <h2 className="text-black text-2xl font-bold" id="A">Q</h2>
+            <ul className="list-disc pl-4  ml-5 text-black">
+                <li id="list"><a href="https://blog.apilayer.com/query-parameters-what-are-query-parameters/">Query Parameters</a>
+                </li>
+                <li id="list">Quotes</li>
+            </ul>
+            <h2 className="text-black text-2xl font-bold" id="A">R</h2>
+            <ul className="list-disc pl-4  ml-5 text-black">
+                <li id="list"><a
+                    href="https://blog.apilayer.com/api-glossary-introduction-to-the-application-programming-interfaces/#rest">REST</a>
+                </li>
+                <li id="list"><a
+                    href="https://blog.apilayer.com/api-glossary-introduction-to-the-application-programming-interfaces/#restful-api">RESTful
+                    API</a></li>
+                <li id="list">remote ip address</li>
+                <li id="list">Rate Limiting</li>
+            </ul>
+            <h2 className="text-black text-2xl font-bold" id="A">S</h2>
+            <ul className="list-disc pl-4  ml-5 text-black">
+                <li id="list">Static IP Address</li>
+                <li id="list">Static Website</li>
+                <li id="list">Statistics Endpoint</li>
+                <li id="list"><a href="https://blog.apilayer.com/scraping-google-what-is-google-scraping/">Scraping Google</a>
+                </li>
+                <li id="list"><a href="https://blog.apilayer.com/sms-api-the-simplest-way-to-send-and-receive-texts/">SMS API</a>
+                </li>
+                <li id="list">SSL</li>
+            </ul>
+            <h2 className="text-black text-2xl font-bold" id="A">T</h2>
+            <ul className="list-disc pl-4  ml-5 text-black">
+                <li id="list">TechLayer</li>
+                <li id="list"><a
+                    href="https://blog.apilayer.com/api-glossary-introduction-to-the-application-programming-interfaces/#tcp">TCP</a>
+                </li>
+                <li id="list">Third-party APIs</li>
+                <li id="list">Third-party API</li>
+                <li id="list"><a
+                    href="https://blog.apilayer.com/how-to-translate-documents-with-google-translate-api/#:~:text=What%20is%20Translation%20API%3F">Translation
+                    API</a></li>
+            </ul>
+            <h2 className="text-black text-2xl font-bold" id="A">U</h2>
+            <ul className="list-disc pl-4  ml-5 text-black">
+                <li id="list">Usage Stats</li>
+                <li id="list"><a
+                    href="https://blog.apilayer.com/validate-an-email-what-is-email-validation/#:~:text=What%20is%20User%20Authentication%3F">User
+                    Authentication</a></li>
+            </ul>
+            <h2 className="text-black text-2xl font-bold" id="A">W</h2>
+            <ul className="list-disc pl-4  ml-5 text-black">
+                <li id="list">Weatherstack API</li>
+                <li id="list">Webhook</li>
+                <li id="list">web scraping</li>
+                <li id="list">Website</li>
+                <li id="list">Website Design</li>
+                <li id="list"><a
+                    href="https://blog.apilayer.com/what-is-whois/#:~:text=It%20is%20an%20ICANN%2D,-owned%20system%20that">Whois</a>
+                </li>
+                <li id="list"><a
+                    href="https://blog.apilayer.com/workspace-one-what-is-workspace-one/#:~:text=What%20is%20Workspace%20ONE%3F">Workspace
+                    ONE</a></li>
+            </ul>
+
+            <h2 className="text-black text-2xl font-bold" id="A">X</h2>
+            <ul className="list-disc pl-4  ml-5 text-black">
+                <li id="list"><a
+                    href="https://blog.apilayer.com/api-glossary-introduction-to-the-application-programming-interfaces/#xml">XML</a>
+                </li>
+            </ul>
+            <h2 className="text-black text-2xl font-bold" id="A">Y</h2>
+            <ul className="list-disc pl-4  ml-5 text-black">
+                <li id="list"><a
+                    href="https://blog.apilayer.com/api-glossary-introduction-to-the-application-programming-interfaces/#yaml">YAML</a>
+                </li>
+            </ul>
+        </article>
+    )
+}
